bac485aa5e3b9fb3a54b6707d364f10e867f96a1
ztd5049 <ztd5049@psu.edu>
2023-11-06 17:33:37 -0500

    fixing main.js

--- ThreeJS-project/dist/main.js
+++ ThreeJS-project/dist/main.js
@@ -1,9 +1,8 @@
-import './style.css'
 import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
-// Setup!
+// Setup!greg
     
 const scene = new THREE.Scene();
         
