14fdd9795ddaace7d17f6f4f63ddd3fab4848c50
ztd5049 <ztd5049@psu.edu>
2023-11-06 17:29:17 -0500

    fixing main.js

--- ThreeJS-project/dist/main.js
+++ ThreeJS-project/dist/main.js
@@ -1,4 +1,3 @@
-import * as THREE from 'three';
 import './style.css'
 import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
--- ThreeJS-project/main.js
+++ ThreeJS-project/main.js
@@ -1,9 +1,5 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
-
-//import * as THREE from 'three';
-//import './style.css'
+import * as THREE from 'three';
+import './style.css'
 
 // Setup
     
